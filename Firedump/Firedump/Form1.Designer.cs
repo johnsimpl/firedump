1a97b20f1631f7d2e9457ad2f34f2a24bb01a933
abraham <piperidis3@yahoo.gr>
2016-10-28 01:29:25 +0300

    commit

--- Firedump/Firedump/Form1.Designer.cs
+++ Firedump/Firedump/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(133, 39);
+            this.button2.Location = new System.Drawing.Point(37, 88);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
