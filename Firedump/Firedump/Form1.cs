f886d3ed4240462ef4e600e90a8e9d3c7282f83e
cspeitch <valadispit2@hotmail.com>
2016-10-29 06:08:58 +0200

    copy db to build location

--- Firedump/Firedump/Form1.cs
+++ Firedump/Firedump/Form1.cs
@@ -29,10 +29,10 @@ namespace Firedump
         private void button1_Click(object sender, EventArgs e)
         {
 
-            //FiredumpContext f = new FiredumpContext();
-            //List<mysql_servers> s = f.getAllMySqlServers();
+            FiredumpContext f = new FiredumpContext();
+            List<mysql_servers> s = f.getAllMySqlServers();
             //string newdpath = Environment.GetFolderPath(Environment.SpecialFolder.Da);
-            //Console.WriteLine(s.Count);
+            Console.WriteLine(s.Count);
             
         }
 
