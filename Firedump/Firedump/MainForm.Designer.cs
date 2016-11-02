6d900f61ac8598ec317c2ea98a98397b14b8489b
xinyu_000 <xinyu_000@LENOVO-PC>
2016-11-02 22:00:24 +0200

    email

--- Firedump/Firedump/MainForm.Designer.cs
+++ Firedump/Firedump/MainForm.Designer.cs
@@ -31,6 +31,7 @@
             this.bDumpForm = new System.Windows.Forms.Button();
             this.bTest1 = new System.Windows.Forms.Button();
             this.bGenConfig = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bDumpForm
@@ -63,11 +64,22 @@
             this.bGenConfig.UseVisualStyleBackColor = true;
             this.bGenConfig.Click += new System.EventHandler(this.bGenConfig_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "email";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.email_click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.bGenConfig);
             this.Controls.Add(this.bTest1);
             this.Controls.Add(this.bDumpForm);
@@ -82,5 +94,6 @@
         private System.Windows.Forms.Button bDumpForm;
         private System.Windows.Forms.Button bTest1;
         private System.Windows.Forms.Button bGenConfig;
+        private System.Windows.Forms.Button button1;
     }
 }
