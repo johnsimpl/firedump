4b8f061bfff78aef8e9a175d021765051fa24f5c
avraampiperidis <piperidis3@yahoo.gr>
2016-11-03 21:59:22 +0200

    sqlviewer form started

--- Firedump/Firedump/MainForm.Designer.cs
+++ Firedump/Firedump/MainForm.Designer.cs
@@ -32,6 +32,7 @@
             this.bTest1 = new System.Windows.Forms.Button();
             this.bGenConfig = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bDumpForm
@@ -74,11 +75,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.email_click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "sql viewer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.bGenConfig);
             this.Controls.Add(this.bTest1);
@@ -95,5 +107,6 @@
         private System.Windows.Forms.Button bTest1;
         private System.Windows.Forms.Button bGenConfig;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
